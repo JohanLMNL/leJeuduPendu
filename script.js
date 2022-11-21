fcd5806d3c1a0aad98e0edfe4271a3525b1b48f6
JohanLMNL <j.lemesnil@gmail.com>
2022-11-21 14:17:41 +0100

    mise en place du design général et todolist

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1_ Liste Des mots à trouver
+// 2_ Selection d'un mot aléatoirement dans la liste
+// 3_ Affichage du mot selectionner sous forme de tirets
+// 4_ Clavier qui reconnait les lettres sur lesquelles on tape
+// 5_ Affichage du mot en fonction des lettres sur lesquelles on tape
+// 6_ "Bloquer" les lettres déjà utilisées
+// 7_ Donner un nombre de coups limités pour trouver le mot
+// 8_ Compter les points et game over aprés un certain nombre de coups
+// 9_ Afficher le bonhomme Pendu
+// 10_On ajoute la possibilité de mettre le mot (-1 coups)
+// 11_ Popup "Vous Avez gagner (perdu)" et bouton rejouer
